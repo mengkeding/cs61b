ccac456d295d987826bb1e5e7f23406f51d0c2fc
w1024020103 <w1024020103@gmail.com>
2017-03-09 13:30:26 +0800

    added Row.java representing row object of a table

--- proj2/db/Row.java
+++ proj2/db/Row.java
@@ -1,22 +1,28 @@
 package db;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Iterator;
 import java.io.Serializable;
 
-    /**Implement the classes to manage rows, namely row, rowDesc. We have already implemented column, Intcolumn, Stringcolumn, and Type for you. Since you only need to support integer and (fixed length) string columns and fixed length rows, these are straightforward.
+    /**Implement the classes to manage rows, namely row, rowDesc. We have already implemented column, IntColumn, StringColumn, and Type for you.
+     * Since you only need to support integer and (fixed length) string columns and fixed length rows, these are straightforward.
      Implement the Catalog (this should be very simple).
      Implement the BufferPool constructor and the getPage() method.
      Implement the access methods, HeapPage and HeapFile and associated ID classes. A good portion of these files has already been written for you.
      Implement the operator SeqScan.
      At this point, you should be able to pass the ScanTest system test, which is the goal for this lab.
      Section 2 below walks you through these implementation steps and the unit tests corresponding to each one in more detail.
+
      * row maintains information about the contents of a row. rows have a
      * specified schema specified by a rowDesc object and contain column objects
      * with the data for each column.
      */
     public class Row implements Serializable {
 
-        private static final long serialVersionUID = 1L;
+
+        public final RowDesc schema;
+        public List<Column> RowList = new ArrayList<Column>();
 
         /**
          * Create a new row with the specified schema (type).
@@ -27,14 +33,40 @@ import java.io.Serializable;
          */
         public Row(RowDesc rd) {
             // some code goes here
+          this.schema = rd;
+          for(int i = 0; i < rd.rdItemList.size(); i++){
+              if(rd.rdItemList.get(i).columnType == Type.INT){
+                  IntColumn intCol = new IntColumn();
+                  RowList.add(intCol);
+              }else if(rd.rdItemList.get(i).columnType == Type.STRING){
+                  StringColumn strCol = new StringColumn();
+                  RowList.add(strCol);
+              }
+          }
         }
+
+
+
+
+//        public IntColumn(int i) {
+//            value = i;
+//        }
+//        public StringColumn(String s, int maxSize) {
+//            this.maxSize = maxSize;
+//
+//            if (s.length() > maxSize)
+//                value = s.substring(0, maxSize);
+//            else
+//                value = s;
+//        }
+
 
         /**
          * @return The rowDesc representing the schema of this row.
          */
         public RowDesc getrowDesc() {
             // some code goes here
-            return null;
+            return this.schema;
         }
 
         /**
@@ -45,13 +77,13 @@ import java.io.Serializable;
 //            // some code goes here
 //            return null;
 //        }
-
-        /**
-         * Set the RecordId information for this row.
-         *
-         * @param rid
-         *            the new RecordId for this row.
-         */
+//
+//        /**
+//         * Set the RecordId information for this row.
+//         *
+//         * @param rid
+//         *            the new RecordId for this row.
+//         */
 //        public void setRecordId(RecordId rid) {
 //            // some code goes here
 //        }
@@ -61,11 +93,16 @@ import java.io.Serializable;
          *
          * @param i
          *            index of the column to change. It must be a valid index.
-         * @param f
+         * @param c
          *            new value for the column.
          */
-        public void setcolumn(int i, Row f) {
+        public void setColumn(int i, Column c) {
             // some code goes here
+            if(i < 0 || i > schema.rdItemList.size()){
+                throw new IllegalArgumentException("invalid index");
+            }else{
+                this.RowList.set(i,c);
+            }
         }
 
         /**
@@ -74,29 +111,48 @@ import java.io.Serializable;
          * @param i
          *            column index to return. Must be a valid index.
          */
-        public Row getRow(int i) {
+        public Column getColumn(int i) {
             // some code goes here
-            return null;
+            if(i < 0 || i > schema.rdItemList.size()){
+                throw new IllegalArgumentException("invalid index");
+            } else {
+                return this.RowList.get(i);
+            }
         }
 
         /**
          * Returns the contents of this row as a string. Note that to pass the
          * system tests, the format needs to be as follows:
          *
-         * column1\tcolumn2\tcolumn3\t...\tcolumnN\n
+         * 'Maurice','Lee','Steelers'
+         * 'New York',2015,1
          *
          * where \t is any whitespace, except newline, and \n is a newline
          */
         public String toString() {
             // some code goes here
-            throw new UnsupportedOperationException("Implement this");
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0 ; i < RowList.size(); i++){
+                if (this.RowList.get(i).getType() == Type.INT){
+                    sb.append(this.RowList.get(i));
+                    sb.append(",");
+                }else if(this.RowList.get(i).getType() == Type.STRING){
+                    sb.append("'");
+                    sb.append(this.RowList.get(i);)
+                    sb.append("'");
+                    sb.append(",");
+                }
+            }
+            return sb.toString();
         }
+
+
 
         /**
          * @return
          *        An iterator which iterates over all the columns of this row
          * */
-        public Iterator<Row> rows(){
+        public Iterator<Column> columns(){
             // some code goes here
             return null;
         }
@@ -106,7 +162,9 @@ import java.io.Serializable;
          * */
         public void resetRowDesc(RowDesc td)
         {
-            // some code goes here
+            // so
+            // me code goes here
+
         }
     }
 
